1bddb90ecc2550b6da03dbfd38c33bb416f6590e
zhangya998@gmail.com <zhangya998@gmail.com@f4293942-5c4c-380f-1b81-e85389bcc170>
2011-01-31 07:06:57 +0000

    升级完成

--- trunk/common.php
+++ trunk/common.php
@@ -10,9 +10,9 @@
 		echo "<script type='text/javascript'>window.location.replace('" . DOMAIN . $location . "');</script>";
 	}
 	
-	function htmlhead($title="undefined")
+	function htmlhead($title="undefined",$ui="nui")
 	{
-		global $jquerylib,$jslib;
+		global $jquerylib,$jslib,$jqueryui,$jqueryuitheme;
 		echo <<< htmlhead
 			<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 			<html xmlns="http://www.w3.org/1999/xhtml">
@@ -21,10 +21,20 @@
 			    <link type="text/css" href="css/all.css" rel="stylesheet" />
 				<script type="text/javascript" src="$jquerylib"></script>
 				<script type="text/javascript" src="$jslib/conf.js"></script>
+htmlhead;
+		if ($ui="ui")
+		{
+			echo "<script type='text/javascript' src='$jqueryui'></script>";
+			echo "<link type='text/css' href='$jqueryuitheme' rel='stylesheet' />";
+		}
+		
+		echo <<< head
+				<script type="text/javascript" src="$jqueryui"></script>
 				<title>$title</title>
 			</head>
 			<body>
-htmlhead;
+head;
+
 	}
 	
 	function htmltail()
